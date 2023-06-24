114691689e196f9dee4eb3a88cd0ce44c04d8ac3
Murilo Scheunemann <murilo.scheunemann44@gmail.com>
2023-06-24 14:05:49 -0300

    Aperfeiçoamento do layout da página

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ function App() {
 
       {/* ****** TUDO ******* */}
       <div className="row">
-        {/* FORMULARIOS --> col-md-6*/}
-        <div className="col-md-5 m-5">
+        {/* FORMULARIOS --> col-md-5*/}
+        <div className="col-md-5 m-5 border border-2 border-primary-subtle p-5 rounded">
           <form>
             <div className="mb-3 me-5">
               <label htmlForfor="codProduto" className="form-label fw-bold">Código do Produto:</label>
@@ -47,42 +47,51 @@ function App() {
                 <input className="form-control" id="valorTot" />
               </div>
             </div>
-            <button type="submit" className="btn btn-primary ps-5 pe-5">Incluir</button>
+            <div className="d-grid gap-2 col-6 mx-auto mt-5">
+              <button type="submit" className="btn btn-primary btn-lg ps-5 pe-5">Incluir</button>
+            </div>
           </form>
         </div>
 
         {/* LISTA E BOTOES --> col-md-6 + 2x row*/}
         <div className="col-md-6 mt-5">
           {/* TABELA*/}
-          <div className="row">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Quantidade</th>
-                  <th scope="col">Código</th>
-                  <th scope="col">Produto</th>
-                  <th scope="col">Valor Unitário</th>
-                  <th scope="col">Valor Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>xxx</td>
-                  <td>xxx</td>
-                  <td>xxx</td>
-                  <td>xxx</td>
-                  <td>xxx</td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="row ">
+            <div className=" border border-2 border-primary-subtle rounded py-4">
+
+              <table className="table p-2">
+                <thead>
+                  <tr>
+                    <th scope="col">Quantidade</th>
+                    <th scope="col">Código</th>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Valor Unitário</th>
+                    <th scope="col">Valor Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>xxx</td>
+                    <td>xxx</td>
+                    <td>xxx</td>
+                    <td>xxx</td>
+                    <td>xxx</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
 
-          <p className="ms-2 me-5 fs-2">Subtotal: <span>R$ xx,xx</span></p>
+          <div className="border border-2 border-primary-subtle rounded mb-5 mt-3 pt-2 ps-2">
+            <p className="fs-2 p-1 fw-light">Subtotal: <span>R$</span></p>
+          </div>
 
           {/* BOTOES */}
           <div className="row">
-          <button className="btn btn-primary mb-3 mt-5" type="submit">Finalizar</button>
-          <button className="btn btn-danger" type="submit">Cancelar</button>
+            <div className="col-sm-12 offset-md-6">
+              <button className="btn btn-danger btn-lg px-5" type="submit">Cancelar</button>
+              <button className="btn btn-success btn-lg ms-4 px-5" type="submit">Finalizar</button>
+            </div>
           </div>
         </div>
 
